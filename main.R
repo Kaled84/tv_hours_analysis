f8bcfcfa983d94c574e92918d47a566ada837215
Kaled84 <kaledisma84@gmail.com>
2026-07-06 17:44:34 +0300

    Update main.R

--- main.R
+++ main.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 
 tv_hours_table<- gss_cat %>% 
-    filter(age < 30) %>% 
+    filter(age >= 30) %>% 
     group_by(marital) %>% 
     summarise(avg_tv_hours= mean(tvhours, na.rm= T))
 
